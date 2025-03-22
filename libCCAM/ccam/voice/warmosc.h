23eb95cf9090dd18a05d0641938d6db40467340d
Garnet Grimm <garnetgrimm720@gmail.com>
2025-03-22 10:37:51 -0500

    remove unused

--- libCCAM/ccam/voice/warmosc.h
+++ libCCAM/ccam/voice/warmosc.h
@@ -10,19 +10,6 @@ class WarmOsc : public daisysp::Oscillator {
     float root_freq = 0.0f;
 public:
     void Init(float sample_rate);
-    /*
-    inline void SetFreq(const float f);
-    inline void SetAmp(const float a);
-    inline void SetWaveform(const uint8_t wf);
-    inline void SetPw(const float pw);
-    inline bool IsEOR();
-    inline bool IsEOC();
-    inline bool IsRising();
-    inline bool IsFalling();
-    float Process();
-    void PhaseAdd(float _phase);
-    void Reset(float _phase = 0.0f);
-    */
     void SetDetuneAmt(float amt);
     void SetDetuneFreq(float freq);
     void SetRootFreq(float freq);
